de350d1ba5ebbed8b5d7ab3a964d05c909401e0d
zhouxiaoting <1563656602@qq.com>
2016-12-24 22:45:16 +0800

    Merge branch 'master' of https://github.com/yaobin1340/sky_frontend
    # Conflicts:
    #	app/app.js
    #	views/home.html

--- controller/list-orders-ctrl.js
+++ controller/list-orders-ctrl.js
@@ -10,6 +10,30 @@ angular
         var $mdDialog = $injector.get('$mdDialog');
         var $mdMedia = $injector.get('$mdMedia');
         var $mdToast = $injector.get('$mdToast');
+            $scope.oneAtATime = true;
 
+            $scope.groups = [
+                    {
+                            title: 'Dynamic Group Header - 1',
+                            content: 'Dynamic Group Body - 1'
+                    },
+                    {
+                            title: 'Dynamic Group Header - 2',
+                            content: 'Dynamic Group Body - 2'
+                    }
+            ];
+
+            $scope.items = ['Item 1', 'Item 2', 'Item 3'];
+
+            $scope.addItem = function() {
+                    var newItemNo = $scope.items.length + 1;
+                    $scope.items.push('Item ' + newItemNo);
+            };
+
+            $scope.status = {
+                    isCustomHeaderOpen: false,
+                    isFirstOpen: true,
+                    isFirstDisabled: false
+            };
 
     });
