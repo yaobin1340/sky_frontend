40f4a547a93be61dad5a1d0ff9fbacc830409e1d
zhouxiaoting <1563656602@qq.com>
2017-01-05 17:33:54 +0800

    Merge branch 'master' of https://github.com/yaobin1340/sky_frontend
    # Conflicts:
    #	app/app.js
    #	views/home.html

--- controller/product-detail-ctrl.js
+++ controller/product-detail-ctrl.js
@@ -30,4 +30,24 @@ angular
             });
 
 
+
+
+        $scope.rate = 7;
+        $scope.max = 5;
+        $scope.isReadonly = false;
+
+        $scope.hoveringOver = function(value) {
+            $scope.overStar = value;
+            $scope.percent = 100 * (value / $scope.max);
+        };
+
+        $scope.ratingStates = [
+            {stateOn: 'glyphicon-ok-sign', stateOff: 'glyphicon-ok-circle'},
+            {stateOn: 'glyphicon-star', stateOff: 'glyphicon-star-empty'},
+            {stateOn: 'glyphicon-heart', stateOff: 'glyphicon-ban-circle'},
+            {stateOn: 'glyphicon-heart'},
+            {stateOff: 'glyphicon-off'}
+        ];
+
+
     });
